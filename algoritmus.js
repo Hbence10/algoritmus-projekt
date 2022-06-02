d0e82a5af674904117782f5a4047183cae24c964
Hbence10 <93594678+Hbence10@users.noreply.github.com>
2022-06-02 21:03:40 +0200

    start write javascript with partner

---
+++ algoritmus.js
@@ -0,0 +1,16 @@
+const gomb = document.getElementById('gomb');
+gomb.addEventListener("click", szamitas);
+
+let hcf;
+
+function szamitas (){
+const number1 = document.getElementById('szam1').value;
+const number2 = document.getElementById('szam2').value;
+for (let i = 1; i <= number1 && i <= number2; i++) {
+    if( number1 % i == 0 && number2 % i == 0) {
+        hcf = i;
+        }
+}
+    document.getElementById('eredmeny').innerHTML = `HCF of ${number1} and ${number2} is ${hcf}.`;
+
+}
