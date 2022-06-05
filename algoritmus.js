fbfccd4594ab781c1ce70dbb198887c5a3262546
Hbence10 <93594678+Hbence10@users.noreply.github.com>
2022-06-05 21:03:43 +0200

    finish html and css

--- algoritmus.js
+++ algoritmus.js
@@ -9,8 +9,6 @@ const number2 = document.getElementById('szam2').value;
 for (let i = 1; i <= number1 && i <= number2; i++) {
     if( number1 % i == 0 && number2 % i == 0) {
         hcf = i;
-        }
-}
-    document.getElementById('eredmeny').innerHTML = `HCF of ${number1} and ${number2} is ${hcf}.`;
-
+        }   
+    }
 }
